2b72beccf470a7ab42fa0281861aac3bbbe7da63
Valtear <larionov.alex93@gmail.com>
2023-05-25 11:24:33 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/CustomDBConnection.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    internal class CustomDBConnection
+    {
+        string connectionString;
+        string query;
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataReader reader;
+        public CustomDBConnection()
+        {
+            this.connectionString = null;
+            this.query = null;
+            this.connection = null;
+            this.command = null;
+            this.reader = null;
+        }
+        public string ConnectionString
+        {
+            get { return this.connectionString; }
+            set
+            {
+                this.connectionString = value;
+                this.connection = new SqlConnection(value);
+            }
+        }
+        public string Query
+        {
+            get { return this.query; }
+            set
+            {
+                this.query = value;
+                this.command = new SqlCommand(value, this.connection);
+            }
+        }
+        public SqlDataReader Reader
+        {
+            get { return this.reader; }
+        }
+        public void Execute()
+        {
+            this.reader = command.ExecuteReader();
+        }
+        public void Open()
+        {
+            connection.Open();
+        }
+        public void Close()
+        {
+            connection.Close();
+        }
+        public bool Read()
+        {
+            return this.reader.Read();
+        }
+    }
+}
---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CustomDBConnection myConnection = new CustomDBConnection();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            myConnection.ConnectionString = @"Data Source=DESKTOP-CSA0P33\SQLEXPRESS;Initial Catalog=FruitsVeg;Integrated Security=True";
+            myConnection.Query = @"Select * from Fru_Veg";
+            try
+            {
+                myConnection.Open();
+                MessageBox.Show("connection established");
+                myConnection.Execute();
+                while (myConnection.Read())
+                {
+                    lbName.Items.Add(myConnection.Reader[1]);
+                    lbType.Items.Add(myConnection.Reader[2]);
+                    lbColor.Items.Add(myConnection.Reader[3]);
+                    lbKK.Items.Add(myConnection.Reader[4]);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Connection not established");
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+        }
+
+    }
+}
